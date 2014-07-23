220dee8b2b332835bd34eeea4057e8f1b7ea16f3
Soyoon <soyoon0918@nate.com>
2014-07-23 11:06:18 +0900

    본문 페이지 레이아웃

---
+++ src/com/nexters/house/activity/ContentDetailActivity.java
@@ -0,0 +1,25 @@
+package com.nexters.house.activity;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+
+import com.nexters.house.*;
+
+public class ContentDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.content_detail_page);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+
+}
