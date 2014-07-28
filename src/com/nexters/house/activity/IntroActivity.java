31e227a6301ef7b2c14970ebefd7201df921d4a6
Soyoon <soyoon0918@nate.com>
2014-07-28 16:12:36 +0900

    인트로 화면

---
+++ src/com/nexters/house/activity/IntroActivity.java
@@ -0,0 +1,41 @@
+package com.nexters.house.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.nexters.house.R;
+import com.nexters.house.core.AccountManager;
+
+public class IntroActivity extends Activity {
+	private static int INTRO_LOADING_TIME = 1000;
+    private Handler mHandler = null;
+
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_intro);
+
+		mHandler = new Handler();
+		mHandler.postDelayed(new Runnable() {
+            public void run() {
+                endIntro();
+            }
+        }, INTRO_LOADING_TIME);
+	}
+
+	private void endIntro() {
+        if (AccountManager.getInstance().isSignedIn(this)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+	}
+}
