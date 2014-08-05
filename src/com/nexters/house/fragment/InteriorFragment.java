94115c6f5a2e2a2de197a619bd385639a52ba221
Soyoon <soyoon0918@nate.com>
2014-08-05 20:39:34 +0900

    인테리어 리스트뷰 스레드 핸들러 추가

--- src/com/nexters/house/fragment/InteriorFragment.java
+++ src/com/nexters/house/fragment/InteriorFragment.java
@@ -8,7 +8,7 @@ import android.support.v4.app.*;
 import android.util.*;
 import android.view.*;
 import android.widget.*;
-import android.widget.AbsListView.*;
+import android.widget.AbsListView.OnScrollListener;
 
 import com.nexters.house.*;
 import com.nexters.house.activity.*;
@@ -50,8 +50,9 @@ public class InteriorFragment extends Fragment{
 		//add the footer before adding the adapter, else the footer will nod load!
 		View footerView = ((LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.listfooter, null, false);
 		
-		//getListView() : 리스트 액티비티의 멤버인 리스트뷰 객체를 조사
+		//footerview를  listview 제일 하단에 붙임 
 		lv_main.addFooterView(footerView);
+		
 		mInteriorItemArrayList = new ArrayList<InteriorEntity>();
 		
 		mListAdapter = new InteriorAdapter(getActivity().getApplicationContext(),
@@ -114,7 +115,6 @@ public class InteriorFragment extends Fragment{
 			public void onClick(View v) {
 	    		mListAdapter.add();
 	    		mListAdapter.notifyDataSetChanged();
-	    		
 				
 			}
 	    	
@@ -131,26 +131,87 @@ public class InteriorFragment extends Fragment{
 			public void onScroll(AbsListView view, int firstVisibleItem,
 					int visibleItemCount, int totalItemCount) {
 				
-			       if (loading) {
-			            if (totalItemCount > previousTotal) {
-			                loading = false;
-			                previousTotal = totalItemCount;
-			                currentPage++;
-			            }
-			        }
-			        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
-			            // I load the next page of gigs using a background task,
-			            // but you can call any function here.
-			            mListAdapter.add();
-			            mListAdapter.notifyDataSetChanged();
-			            loading = true;
-			        }
+				
+				//what is the bottom iten that is visible
+				int lastInScreen = firstVisibleItem + visibleItemCount;				
+				
+				//is the bottom item visible & not loading more already ? Load more !
+				if((lastInScreen == totalItemCount) && !(loading)){					
+					Thread thread =  new Thread(null, loadMoreListItems);
+			        thread.start();
+				}
 			}
 		});
 	    
+		//Load the first 20 items
+		Thread thread =  new Thread(null, loadMoreListItems);
+        thread.start();
+	    
 	}
 
+	   //Runnable to load the items 
+    private Runnable loadMoreListItems = new Runnable() {			
+		@SuppressWarnings("serial")
+		@Override
+		public void run() {
+			//Set flag so we cant load new items 2 at the same time
+			loading = true;
+			
+			//Reset the array that holds the new items
+			mInteriorItemArrayList = new ArrayList<InteriorEntity>();
+	    	
+			//Simulate a delay, delete this on a production environment!
+	    	try { Thread.sleep(1000);
+			} catch (InterruptedException e) {}
+			
+			//Get 15 new listitems
+	    	for (int i = 0; i < 20; i++) {		
+				mListAdapter.add();
+				mHandler.sendEmptyMessage(0);
+			}
+	    	
+	    	
+			//Done! now continue on the UI thread
+	        getActivity().runOnUiThread(returnRes);
+	        
+		}
+	};	
+	
+    
+	//Since we cant update our UI from a thread this Runnable takes care of that! 
+	//스레드는 내부적인 연산만 해야한다. 다른 스레드 소속의 UI를 건드릴 수 없다. 그래서 Handler를 사용하는거?!
+	//스레드간의 통신할 수 있는 장치가 필요한데 그게 바로 핸들러이다.
+	
+	
+    private Runnable returnRes = new Runnable() {
+        @Override
+        public void run() {
+        	
+			//Loop thru the new items and add them to the adapter
+			if(mInteriorItemArrayList != null && mInteriorItemArrayList.size() > 0){
+                for(int i=0;i<mInteriorItemArrayList.size();i++)
+                	mListAdapter.add();
+            }
+        	
+			//Update the Application title
+        	getActivity().setTitle("House" + String.valueOf(mListAdapter.getCount()) + " items");
+			
+			//Tell to the adapter that changes have been made, this will cause the list to refresh
+        	mListAdapter.notifyDataSetChanged();
+			
+			//Done loading more.
+            loading = false;
+        }
+    };
 
+    
+    Handler mHandler = new Handler(){
+    	public void handleMessage(Message msg){
+    		if(msg.what == 0){
+    			mListAdapter.notifyDataSetChanged();
+    		}
+    	}
+    };
 
 }
 
